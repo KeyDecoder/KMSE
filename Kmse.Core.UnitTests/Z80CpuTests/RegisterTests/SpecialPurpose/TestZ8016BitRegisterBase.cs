a7cbc9fc41630d49f9d8c23f01cda5d1bb78a1ab
Tim Pike <17920409+KeyDecoder@users.noreply.github.com>
2022-09-24 14:05:34 +1000

    Add unit tests for Z8016BitRegisterBase Tidied up Substract for Z8016BitRegisterBase where it does not support subtract without carry so removed that as an option Revert accidental commit of launchSettings for console app with local file references

---
+++ Kmse.Core.UnitTests/Z80CpuTests/RegisterTests/SpecialPurpose/TestZ8016BitRegisterBase.cs
@@ -0,0 +1,26 @@
+﻿using Kmse.Core.Memory;
+using Kmse.Core.Z80.Model;
+using Kmse.Core.Z80.Registers;
+using Kmse.Core.Z80.Registers.General;
+
+namespace Kmse.Core.UnitTests.Z80CpuTests.RegisterTests.SpecialPurpose;
+
+public class TestZ8016BitRegisterBase : Z8016BitRegisterBase
+{
+    private Unsigned16BitValue _register;
+
+    public TestZ8016BitRegisterBase(IMasterSystemMemory memory, IZ80FlagsManager flags)
+        : base(memory, flags)
+    {
+        _register = new Unsigned16BitValue();
+    }
+
+    public override ushort Value => _register.Word;
+    public override byte High => _register.High;
+    public override byte Low => _register.Low;
+
+    public override void Set(ushort value)
+    {
+        _register.Word = value;
+    }
+}
