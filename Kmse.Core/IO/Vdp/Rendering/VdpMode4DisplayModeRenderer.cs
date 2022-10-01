20a00df7c79076bff5bc44ef366bcf741770e766
Tim Pike <17920409+KeyDecoder@users.noreply.github.com>
2022-10-01 16:18:21 +1000

    Added initial implementation of rendering background just rendering the tiles for now, no scrolling, flipping or prioritization of sprite vs background etc Fix issue in sprite rendering where checking if was outside current line was off by 1 so including top of another sprite on some lines

--- Kmse.Core/IO/Vdp/Rendering/VdpMode4DisplayModeRenderer.cs
+++ Kmse.Core/IO/Vdp/Rendering/VdpMode4DisplayModeRenderer.cs
@@ -48,8 +48,8 @@ namespace Kmse.Core.IO.Vdp.Rendering
 
         public void RenderLine()
         {
-            RenderSprites();
             RenderBackground();
+            RenderSprites();
         }
 
         public void UpdateDisplay()
@@ -57,6 +57,81 @@ namespace Kmse.Core.IO.Vdp.Rendering
             RenderAllSpritesInAddressTable();
             RenderAllTilesAndSpritesInMemory();
             _displayUpdater.DisplayFrame(_pixels.Span);
+        }
+
+        private void RenderBackground()
+        {
+            var nameTableBaseAddress = _registers.GetNameTableBaseAddressOffset();
+
+            // Background is 32 8x8 tiles across each line
+            // For each line, we draw the line of each tile
+
+            // Tile index depends on the tile line we are on x 32
+            var tileRow = ((byte)Math.Floor((double)_verticalCounter.Counter / 8));
+            var tileIndex = tileRow * 32;
+            for (var tileX = 0; tileX < 32; tileX++)
+            {
+                var address = (ushort)(nameTableBaseAddress + (tileIndex * 2));
+                var firstByte = _ram.ReadRawVideoRam(address);
+                address++;
+                var secondByte = _ram.ReadRawVideoRam(address);
+                var tileInformation = Bitwise.ToUnsigned16BitValue(secondByte, firstByte);
+
+                //MSB LSB
+                //---pcvhnnnnnnnnn
+
+                //    - = Unused.Some games use these bits as flags for collision and damage
+                //zones. (such as Wonderboy in Monster Land, Zillion 2)
+                //p = Priority flag.When set, sprites will be displayed underneath the background pattern in question.
+                //    c = Palette select.
+                //    v = Vertical flip flag.
+                //    h = Horizontal flip flag.
+                //    n = Pattern index, any one of 512 patterns in VRAM can be selected.
+                var priority = Bitwise.IsSet(tileInformation, 12);
+                var secondPalette = Bitwise.IsSet(tileInformation, 11);
+                var verticalFlip = Bitwise.IsSet(tileInformation, 10);
+                var horizontalFlip = Bitwise.IsSet(tileInformation, 9);
+                var patternIndex = (byte)(tileInformation & 0xFF);
+                var patternAddress = (ushort)(patternIndex * 32);
+
+                var xOffset = tileX * 8;
+                var yOffset = _verticalCounter.Counter;
+                var tileYOffset = _verticalCounter.Counter - tileRow * 8;
+
+                RenderTilePatternLine(_pixels, secondPalette, patternAddress, 8, xOffset, tileYOffset, yOffset);
+
+                tileIndex++;
+            }
+        }
+        
+        private void RenderSprites()
+        {
+            var spriteWidth = (byte)_registers.GetSpriteWidth();
+            var sprites = GetSpritesToDraw();
+
+            // Now we have a list of sprites to draw, draw them all, but draw them in reverse order
+            //The inter-sprite priority is defined by the order of the sprites in the
+            // internal buffer.An opaque pixel from a lower-entry sprite is displayed
+            // over any opaque pixel from a higher-entry sprite.
+
+            //For example: Out of the eight possible sprites, 5 are used.
+            //Sprites 1,2,3 have transparent pixels.Sprites 4 and 5 have opaque pixels.
+            //Only the pixel from sprite 4 will be shown since it comes before sprite 5.
+
+            if (sprites.Count == 0)
+            {
+                return;
+            }
+
+            for (var spriteNumber = sprites.Count - 1; spriteNumber >= 0; spriteNumber--)
+            {
+                var sprite = sprites[spriteNumber];
+
+                var patternAddress = (ushort)(_registers.GetSpritePatternGeneratorBaseAddressOffset() + (sprite.PatternIndex * 32));
+
+                var spriteYOffset = _verticalCounter.Counter - sprite.Y;
+                RenderTilePatternLine(_pixels, true, patternAddress, spriteWidth, sprite.X, spriteYOffset, _verticalCounter.Counter);
+            }
         }
 
         public void RenderAllSpritesInAddressTable()
@@ -91,7 +166,7 @@ namespace Kmse.Core.IO.Vdp.Rendering
                 // TODO: Handle 16 pixel sprite height?
                 for (var y = 0; y < spriteHeight; y++)
                 {
-                    RenderSpritePatternLine(pixels, true, patternAddress, spriteWidth, xOffset, y, yOffset+y);
+                    RenderTilePatternLine(pixels, true, patternAddress, spriteWidth, xOffset, y, yOffset+y);
                 }
 
                 spriteCounter++;
@@ -115,9 +190,9 @@ namespace Kmse.Core.IO.Vdp.Rendering
             var spriteWidth = (byte)_registers.GetSpriteWidth();
             var yOffset = 5;
             var spriteCounter = 0;
-            var maxPerLine = 32;
-            var start = 0;
-            var end = start + 288;
+            const int maxPerLine = 32;
+            const int start = 0;
+            const int end = start + 288;
 
             ResetPixels(_debugSpriteTileMemoryPixels, 0x00, 0x00, 0x00);
 
@@ -129,7 +204,7 @@ namespace Kmse.Core.IO.Vdp.Rendering
                 for (var y = 0; y < 8; y++)
                 {
                     // Since we are rendering all the tiles and sprites, we use the tile palette
-                    RenderSpritePatternLine(pixels, false, patternAddress, spriteWidth, xOffset, y, yOffset + y);
+                    RenderTilePatternLine(pixels, true, patternAddress, spriteWidth, xOffset, y, yOffset + y);
                 }
 
                 spriteCounter++;
@@ -142,11 +217,11 @@ namespace Kmse.Core.IO.Vdp.Rendering
             _displayUpdater.DisplayDebugSpriteTileMemory(_debugSpriteTileMemoryPixels.Span);
         }
 
-        private void RenderSpritePatternLine(Memory<byte> frame, bool spritePalette, ushort patternAddress, byte spriteWidth, int xStart, int spriteYOffset, int yline)
+        private void RenderTilePatternLine(Memory<byte> frame, bool secondPalette, ushort patternAddress, byte tileWidth, int xStart, int tileYOffset, int yline)
         {
             // Get offset of 4 bytes for this line
-            var patternLineAddress = (ushort)(patternAddress + (spriteYOffset * 4));
-            var colourAddresses = new byte[spriteWidth];
+            var patternLineAddress = (ushort)(patternAddress + (tileYOffset * 4));
+            var colourAddresses = new byte[tileWidth];
 
             // Is this 8 bytes for zoomed/large?
             var lineData = new byte[4];
@@ -170,24 +245,24 @@ namespace Kmse.Core.IO.Vdp.Rendering
                 pixelOffset++;
             }
 
-            for (var i = 0; i < spriteWidth; i++)
+            for (var i = 0; i < tileWidth; i++)
             {
                 var xCoordinate = xStart + i;
                 if (xCoordinate >= 256)
                 {
-                    // Sprite is partially off screen, so don't draw the rest of the line
+                    // if Tile is partially off screen, so don't draw the rest of the line for the tile
                     break;
                 }
 
-                // Sprites always use the second palette only so we skip the first 16
                 var colourAddress = (ushort)(colourAddresses[i]);
+
                 if (colourAddress == 0)
                 {
                     // Address is 0 which means it is transparent, so don't write anything
                     continue;
                 }
 
-                var colours = GetColor(spritePalette, colourAddress);
+                var colours = GetColor(secondPalette, colourAddress);
 
                 var index = GetPixelIndex(xCoordinate, yline);
                 frame.Span[index + 0] = colours.blue;
@@ -251,8 +326,8 @@ namespace Kmse.Core.IO.Vdp.Rendering
                 spriteY++;
 
                 // Check if Sprites Y is in the current line
-                // This includes the 8x8 pixels of the sprite
-                if (_verticalCounter.Counter < spriteY || _verticalCounter.Counter > spriteY + spriteHeight)
+                // This includes the body pixels of the sprite
+                if (_verticalCounter.Counter < spriteY || _verticalCounter.Counter >= spriteY + spriteHeight)
                 {
                     // Not on line, move to next
                     continue;
@@ -291,107 +366,6 @@ namespace Kmse.Core.IO.Vdp.Rendering
             }
 
             return spritesToDraw;
-        }
-
-        private void RenderSprites()
-        {
-            var spriteWidth = (byte)_registers.GetSpriteWidth();
-            var sprites = GetSpritesToDraw();
-
-            // Now we have a list of sprites to draw, draw them all, but draw them in reverse order
-            //The inter-sprite priority is defined by the order of the sprites in the
-            // internal buffer.An opaque pixel from a lower-entry sprite is displayed
-            // over any opaque pixel from a higher-entry sprite.
-
-            //For example: Out of the eight possible sprites, 5 are used.
-            //Sprites 1,2,3 have transparent pixels.Sprites 4 and 5 have opaque pixels.
-            //Only the pixel from sprite 4 will be shown since it comes before sprite 5.
-
-            if (sprites.Count == 0)
-            {
-                return;
-            }
-
-            for (var spriteNumber = sprites.Count - 1; spriteNumber >= 0; spriteNumber--)
-            {
-                var sprite = sprites[spriteNumber];
-
-                var patternAddress = (ushort)(_registers.GetSpritePatternGeneratorBaseAddressOffset() + (sprite.PatternIndex * 32));
-
-                var spriteYOffset = _verticalCounter.Counter - sprite.Y;
-                RenderSpritePatternLine(_pixels, true, patternAddress, spriteWidth, sprite.X, spriteYOffset, _verticalCounter.Counter);
-
-                ////Each pattern uses 32 bytes.The first four bytes are bitplanes 0 through 3
-                ////for line 0, the next four bytes are bitplanes 0 through 3 for line 1, etc.,
-                ////up to line 7.
-
-                //// Get offset of 4 bytes for this line
-                //var patternLineAddress = (ushort)(patternAddress + ((_verticalCounter.Counter - sprite.Y) * 4));
-                //var colourAddresses = new byte[spriteWidth];
-
-                //// Is this 8 bytes for zoomed/large?
-                //var lineData = new byte[4];
-                //lineData[0] = _ram.ReadRawVideoRam(patternLineAddress++);
-                //lineData[1] = _ram.ReadRawVideoRam(patternLineAddress++);
-                //lineData[2] = _ram.ReadRawVideoRam(patternLineAddress++);
-                //lineData[3] = _ram.ReadRawVideoRam(patternLineAddress);
-
-                //var pixelOffset = 0;
-                //for (var i = 7; i >= 0; i--)
-                //{
-                //    var bit = i;
-                //    Bitwise.SetIf(ref colourAddresses[pixelOffset], 0, () => Bitwise.IsSet(lineData[0], bit));
-                //    Bitwise.SetIf(ref colourAddresses[pixelOffset], 1, () => Bitwise.IsSet(lineData[1], bit));
-                //    Bitwise.SetIf(ref colourAddresses[pixelOffset], 2, () => Bitwise.IsSet(lineData[2], bit));
-                //    Bitwise.SetIf(ref colourAddresses[pixelOffset], 3, () => Bitwise.IsSet(lineData[3], bit));
-                //    pixelOffset++;
-                //}
-
-                //int zoomShift = (_registers.IsSprites16By16() ? 1 : 0);
-                //for (int pixel = 0; pixel < spriteWidth; pixel++)
-                //{
-                //    int c = (((lineData[0] >> (7 - (pixel >> zoomShift))) & 0x1) << 0);
-                //    c |= (((lineData[1] >> (7 - (pixel >> zoomShift))) & 0x1) << 1);
-                //    c |= (((lineData[2] >> (7 - (pixel >> zoomShift))) & 0x1) << 2);
-                //    c |= (((lineData[3] >> (7 - (pixel >> zoomShift))) & 0x1) << 3);
-                //    if (colourAddresses[pixel] != c)
-                //    {
-                //        Console.WriteLine("bad");
-                //    }
-                //    //colourAddresses[pixel] = (byte)c;
-                //}
-
-                //for (var i = 0; i < spriteWidth; i++)
-                //{
-                //    var xCoordinate = sprite.X + i;
-                //    if (xCoordinate >= 256)
-                //    {
-                //        // Sprite is partially off screen, so don't draw the rest of the line
-                //        break;
-                //    }
-
-                //    // Sprites always use the second palette only so we skip the first 16
-                //    if (colourAddresses[i] == 0)
-                //    {
-                //        // Address is 0 which means it is transparent, so don't write anything
-                //        continue;
-                //    }
-
-                //    var colourAddress = (ushort)(colourAddresses[i] + 16);
-                //    var colours = GetColor(colourAddress);
-
-                //    var index = GetPixelIndex(sprite.X + i, _verticalCounter.Counter);
-                //    _pixels.Span[index + 0] = colours.blue;
-                //    _pixels.Span[index + 1] = colours.green;
-                //    _pixels.Span[index + 2] = colours.red;
-                //    _pixels.Span[index + 3] = colours.alpha;
-                //}
-            }
-        }
-
-        private void RenderBackground()
-        {
-
         }
 
         private void ResetPixels(Memory<byte> pixels, byte red, byte green, byte blue)
